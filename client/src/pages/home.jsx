4476445af05efb4d9e6e45d34087e0deff1f85f6
davidwong1358 <dkwck1358@gmail.com>
2022-07-19 21:06:54 +0800

    Update homepage css

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import { Link } from "react-router-dom";
+import '../style/Home.css';
 
 const Slider = () => {
   const [index, setIndex] = useState(0);
